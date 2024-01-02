004bc931a4becba20080aa999c41bffe277baa26
Alabhya Jindal <alabhya10@gmail.com>
2024-01-02 14:04:04 +0530

    Error messages working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    user.save!
-    session[:user_id] = user.id
-    redirect_to posts_path
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
