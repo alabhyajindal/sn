111270c3c53292e190def00dbb0d90bc36d7c459
Alabhya Jindal <alabhya10@gmail.com>
2024-01-02 15:52:32 +0530

    Posts show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    post = current_user.posts.create(post_params)
+    if post.save
+      redirect_to post_path(post)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params[:post].permit(:body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_02_044950) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_093916) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_02_044950) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
