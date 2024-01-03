e8a803bf70372ac988b9078d78a3895814e986a8
Alabhya Jindal <alabhya10@gmail.com>
2024-01-03 12:04:20 +0530

    Update Posts model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def navigation_links
+    [
+      { name: 'new', path: '#' },
+      { name: 'threads', path: '#' },
+      { name: 'past', path: '#' },
+      { name: 'comments', path: '#' },
+      { name: 'ask', path: '#' },
+      { name: 'show', path: '#' },
+      { name: 'jobs', path: '#' },
+      { name: 'submit', path: new_post_path }
+    ]
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def display_url(url)
+    uri = URI.parse(url)
+    uri.host.sub(/\Awww\./, '')
+  end
 end
