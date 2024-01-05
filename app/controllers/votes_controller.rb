262bf61b27d06b042b1a470aa828f1d2b0512c08
Alabhya Jindal <alabhya10@gmail.com>
2024-01-05 11:00:00 +0530

    Remove upvote svg using Turbo Frame

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,11 +3,14 @@ class VotesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-
+  
     if @post.voters << current_user
-      #
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("post-votes-#{@post.id}", partial: "posts/vote_count", locals: { post: @post }) }
+      end
     else
       # 
     end
   end
+  
 end
